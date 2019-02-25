5ecfcac19667a5280f8d1c6584e6cef4b1b9948c
papb <papb1996@gmail.com>
2019-02-25 12:47:16 -0300

    Improve tests

--- cypress/integration/first_spec.js
+++ cypress/integration/first_spec.js
@@ -1,6 +1,13 @@
-describe('Build', function() {    
+describe('Build', function() {
     it('Exports the library to the global namespace', function() {
         cy.visit('cypress/test-entrypoint.html');
         cy.window().should('have.property', 'papbTestFrontendLib');
+    });
+
+    it('Gets the answer correctly', function() {
+        cy.visit('cypress/test-entrypoint.html');
+        cy.window().then(window => {
+            expect(window.papbTestFrontendLib.getTheAnswer()).to.equal(42);
+        });
     });
 });
