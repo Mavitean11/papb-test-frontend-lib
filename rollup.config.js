0473911cada1de92aa563f54c9ca6b408501a667
papb <papb1996@gmail.com>
2019-03-01 00:35:07 -0300

    Update Rollup version

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,35 @@ import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
 
-export default {
-    input: 'index.ts',
-    output: [
-        {
+const external = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+];
+
+const plugins = [
+    typescript({
+        typescript: require('typescript')
+    })
+];
+
+export default [
+    {
+        input: 'index.ts',
+        output: {
             file: pkg.main,
             format: 'umd',
             name: 'papbTestFrontendLib'
         },
-        {
+        external,
+        plugins
+    },
+    {
+        input: 'index.ts',
+        output: {
             file: pkg.module,
             format: 'es'
-        }
-    ],
-    external: [
-        ...Object.keys(pkg.dependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {})
-    ],
-    plugins: [
-        typescript({
-            typescript: require('typescript')
-        })
-    ]
-}
+        },
+        external,
+        plugins
+    }
+]
